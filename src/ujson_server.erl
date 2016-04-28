2a545a722820efbdd1cc6c1cdba396a9ac63c342
Dave Goehrig <dave@dloh.org>
2016-04-28 18:31:17 -0400

    adding license file

--- src/ujson_server.erl
+++ src/ujson_server.erl
@@ -4,13 +4,13 @@
 -behavior(gen_server).
 
 %% public api
--export([ start_link/3, stop/1, define/2 ]).
+-export([ start_link/3, stop/1, send/4 ]).
 
 %% gen_server behavior
 -export([ init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3 ]).
 
 % service identifier
--define(SELF, list_to_atom(?MODULE_STRING ++ "_" ++ integer_to_list(Port))).
+-define(SELF, list_to_atom(?MODULE_STRING ++ "_" ++ integer_to_list(Src))).
 
 % server record
 -record(ujson_server, { port, module, function, socket, schema }).
@@ -19,31 +19,32 @@
 %% Public Behavior
 
 %% start a server
-start_link(Port,Module,Function) ->
+start_link(Src,Module,Function) ->
 	gen_server:start_link({ local, ?SELF }, ?MODULE, #ujson_server{ 
-			port = Port,
+			port = Src,
 			module = Module,
-			function = Function,
-			schema = [] }, []).
+			function = Function
+	}, []).
+
+%% Send
+send(Src,Addr,Port,Message) ->
+	gen_server:call(?SELF, { send, Addr, Port, Message }).
 
 %% Stop a server
-stop(Port) ->
+stop(Src) ->
 	gen_server:call(?SELF, stop).
-
-%% Define a schema
-define(Port,Schema) ->
-	gen_server:call(?SELF, { define, Schema }).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Private Behavior
 
-init(Server = #ujson_server{ port = Port }) ->
-	{ ok, Socket } = gen_udp:open(Port, [ binary, { active, true }]),
+init(Server = #ujson_server{ port = Src }) ->
+	{ ok, Socket } = gen_udp:open(Src, [ binary, { active, true }]),
 	{ ok, Server#ujson_server{ socket = Socket } }.
 
-handle_call({define, Schema }, _From, Server = #ujson_server{
-		schema = Schemas }) ->
-	{ reply, ok, Server#ujson_server{ schema =  [ Schema | Schemas ] }};
+handle_call({ send, Addr, Port, Message }, _From, Server = #ujson_server{
+	socket = Socket }) ->
+	gen_udp:send(Socket,Addr,Port,Message),	
+	{ reply, ok, Server };
 handle_call(stop, _From, Server) ->
 	{ stop, ok, Server };
 handle_call(Message, _From, Server) ->
@@ -56,7 +57,6 @@ handle_cast(Message, Server) ->
 
 handle_info( {udp, Socket, IP, InPortNo, Packet}, Server = #ujson_server{
 	module = Module, function = Function }) ->
-	io:format("ujson_server Got packet form ~p ~p ~p~n", [ Socket, IP, InPortNo ]),
 	spawn(Module,Function,[ Packet ]),
 	{ noreply, Server }.
 
