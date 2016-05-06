e6c88406dc743c2d57cd97694c81f14ef05673f8
Dave Goehrig <dave@dloh.org>
2016-05-06 11:21:49 -0400

    adding eunit test for integers

--- src/ujson.erl
+++ src/ujson.erl
@@ -123,3 +123,41 @@ decode_object(<<>>, Acc) ->
 decode_object(<<Len:16/big-unsigned-integer, Key:Len/binary, Data/binary>>,Acc) ->
 	{ Value, Rem } = decode(Data),
 	decode_object(Rem, [ { Key, Value } | Acc ]).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Tests
+%
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+-compile([debug_info, export_all]).
+test() ->
+	?assertMatch(<< $c, 0>>, ujson:encode(0)),
+	?assertMatch(<< $c, -128>>, ujson:encode(-128)),
+	?assertMatch(<< $c, 127>>, ujson:encode(127)),
+	?assertMatch(<< $C, 128>>, ujson:encode(128)),
+	?assertMatch(<< $C, 255>>, ujson:encode(255)),
+	?assertMatch(<< $C, 255>>, ujson:encode(255)),
+	?assertMatch(<< $w, -129:16/big-integer>>, ujson:encode(-129)),
+	?assertMatch(<< $w, -32768:16/big-integer>>, ujson:encode(-32768)),
+	?assertMatch(<< $W, 32768:16/big-unsigned-integer>>, 
+		ujson:encode(32768)),
+	?assertMatch(<< $W, 65535:16/big-unsigned-integer>>, 
+		ujson:encode(65535)),
+	?assertMatch(<< $i, 65536:16/big-integer>>, 
+		ujson:encode(65536)),
+	?assertMatch(<< $i, -32769:32/big-integer>>, ujson:encode(-32769)),
+	?assertMatch(<< $i, -2147483648/big-integer>>,
+		ujson:encode(-2147483648)),
+	?assertMatch(<< $i, 2147483647/big-integer>>,
+		ujson:encode(2147483647)),
+	?assertMatch(<< $I, 2147483648:32/big-integer>>,
+		ujson:encode(2147483648)),
+	?assertMatch(<< $I, 2147483648:32/big-unsigned-integer>>,
+		ujson:encode(2147483648)),
+	?assertMatch(<< $I, 4294967295:32/big-unsigned-integer>>,
+		ujson:encode(4294967295)),
+	?assertMatch(<< $q, -4294967296:64/big-integer>>,
+		ujson:encode(-4294967296)),
+	?assertMatch(<< $Q, 4294967296:64/big-integer>>,
+		ujson:encode(4294967296)).
+-endif.
